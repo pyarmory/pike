c4136b44042172946ec2c4e77d59ea7f78422332
John Vrbanac <john.vrbanac@linux.com>
2015-07-04 22:36:37 -0500

    Splitting out dunder modification into a separate method

--- pike/loader.py
+++ pike/loader.py
@@ -12,6 +12,17 @@ class PikeLoader(object):
         filename = os.path.basename(self.module_path)
         return filename.startswith('__init__')
 
+    def augment_module(self, fullname, module):
+        package, _, _ = fullname.rpartition('.')
+
+        if self.is_package():
+            module.__path__ = [self.module_path]
+            module.__package__ = fullname
+        else:
+            module.__package__ = package
+
+        return module
+
     def load_module(self, fullname):
         if self.target_module_name != fullname:
             raise ImportError('Cannot import module with this loader')
@@ -20,13 +31,6 @@ class PikeLoader(object):
             return sys.modules[fullname]
 
         module = self.load_module_by_path(fullname, self.module_path)
-        package, _, _ = fullname.rpartition('.')
-
-        if self.is_package():
-            module.__path__ = [self.module_path]
-            module.__package__ = fullname
-        else:
-            module.__package__ = package
 
         sys.modules[fullname] = module
         return module
@@ -38,5 +42,8 @@ class PikeLoader(object):
             module = imp.load_compiled(module_name, path)
         elif ext.lower() == '.py':
             module = imp.load_source(module_name, path)
+
+        # Make sure we properly fill-in __path__ and __package__
+        module = self.augment_module(module_name, module)
 
         return module
