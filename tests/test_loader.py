6315ba171db1116c2da1c10f2cafe93f8d2a0225
John Vrbanac <john.vrbanac@linux.com>
2015-07-04 19:22:55 -0500

    Adding a couple of loader tests

---
+++ tests/test_loader.py
@@ -0,0 +1,35 @@
+import os
+
+from pike.finder import PikeFinder
+from pike.loader import PikeLoader
+from tests import utils
+
+
+SIMPLE_CLASS = """
+class Tracer(object):
+    pass
+"""
+
+class TestLoader(object):
+    def setup_method(self, method):
+        self.temp_folder = utils.make_tmpdir()
+
+        # Create a simple package
+        pkg_location = utils.create_working_package(self.temp_folder)
+        mod_location = os.path.join(pkg_location, 'app.py')
+        utils.write_file(mod_location, SIMPLE_CLASS)
+
+        self.finder = PikeFinder([self.temp_folder])
+        self.loader = PikeLoader('pike_tests.app', mod_location)
+
+    def teardown_method(self, method):
+        utils.remove_dir(self.temp_folder)
+
+    def test_is_package(self):
+        loader = self.finder.find_module('pike_tests')
+        assert loader.is_package()
+
+    def test_module_isnt_package(self):
+        loader = self.finder.find_module('pike_tests.app')
+        assert not loader.is_package()
+
--- tests/utils.py
+++ tests/utils.py
@@ -10,3 +10,20 @@ def make_tmpdir():
 def remove_dir(filename):
     if os.path.exists(filename):
         shutil.rmtree(filename)
+
+
+def write_file(filename, content=''):
+    with open(filename, 'w') as fp:
+        fp.write(content)
+
+
+def create_working_package(location):
+    pkg_location = os.path.join(location, 'pike_tests')
+    subpkg_location = os.path.join(pkg_location, 'sub_package')
+
+    os.makedirs(subpkg_location)
+    write_file(os.path.join(pkg_location, '__init__.py'))
+    write_file(os.path.join(subpkg_location, '__init__.py'))
+
+    return pkg_location
+
