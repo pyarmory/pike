5ae30a1a9c4e0f5c6e4d72434ae3bbe8bc57c23c
Thomas Schraitle <tom_schr@web.de>
2016-05-26 18:22:56 +0200

    Introduce fixture for two test functions
    * Use conftest.py file to introduce fixtures for pytest
    * See test_get_classes_with_fixtures and test_get_inherited_classes_with_fixtures
      in tests/test_manager.py

---
+++ tests/conftest.py
@@ -0,0 +1,61 @@
+#
+#
+#
+
+import pytest
+import textwrap
+
+
+# Default test dir:
+PIKE_TEST_DIR='pike_tests'
+
+
+# ------------------------------------------------------------
+# Fixtures
+#
+# See http://pytest.org/latest/tmpdir.html
+
+@pytest.fixture
+def pike_init_py(tmpdir):
+    """Fixture: Create a directory with an empty __init__.py
+       inside a temporary directory
+
+       :param tmpdir: fixture with py.path.local object
+       :return: temporary directory path to __init__.py
+
+       HINT: Usually you can find the temporary directory under
+             /tmp/pytest-of-$USER/pytest-$NUMBER/$NAME_OF_TEST_FUNCTION/
+    """
+    pkgdir = tmpdir.mkdir(PIKE_TEST_DIR)
+    (pkgdir / "__init__.py").write("")
+    return pkgdir
+
+
+@pytest.fixture
+def pike_tmp_package(pike_init_py):
+    """Fixture: Create a Python package inside a temporary directory.
+       Depends on the pike_init_py fixture
+
+       :param pike_init_py: fixture with py.path.local object pointing to
+                            directory with empty __init__.py
+       :return: temporary directory path to the package (containing __init__.py,
+                app.py, and more.py)
+
+       HINT: Usually you can find the temporary directory under
+             /tmp/pytest-of-$USER/pytest-$NUMBER/$NAME_OF_TEST_FUNCTION/
+    """
+    # First file:
+    (pike_init_py / 'app.py').write(textwrap.dedent("""
+        class SampleObj(object):
+            pass
+
+        class OtherObj(SampleObj):
+            pass
+        """))
+
+    # Second file:
+    (pike_init_py / 'more.py').write(textwrap.dedent("""
+        class AnotherObj(object):
+            pass
+        """))
+    return pike_init_py
