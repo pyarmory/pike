965abc9f89e4035a715590ca0ff647b3789b756b
John Vrbanac <john.vrbanac@linux.com>
2025-05-21 14:58:49 -0500

    Resetting version numbers to 0.1.1 for retag

--- docs/conf.py
+++ docs/conf.py
@@ -50,7 +50,7 @@ author = u'John Vrbanac'
 # built documents.
 #
 # The short X.Y version.
-version = '0.2.0'
+version = '0.1.1'
 # The full version, including alpha/beta/rc tags.
 release = version
 
